a851d2b785a0c06076511b2cb6cd714a4dba789b
daisuke-1202 <75392727+daisuke-1202@users.noreply.github.com>
2023-04-10 23:45:43 +0900

    Add files via upload

---
+++ js/about.6f5f0b6e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkjanken"]=self["webpackChunkjanken"]||[]).push([[443],{8307:function(n,a,e){e.r(a),e.d(a,{default:function(){return k}});var u=e(3396);const t={class:"about"},r=(0,u._)("h1",null,"This is an about page",-1),s=[r];function c(n,a){return(0,u.wg)(),(0,u.iD)("div",t,s)}var i=e(89);const o={},f=(0,i.Z)(o,[["render",c]]);var k=f}}]);
+//# sourceMappingURL=about.6f5f0b6e.js.map
